41bf43f81356d52800a3064114a85ca959e96a2b
ngpalma <nicolasgerardopalma@gmail.com>
2024-02-27 00:36:41 -0300

    add project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import countries from "../assets/Countries captura.png";
 import toolverse from "../assets/ToolVerse captura.png";
 import splendid from "../assets/Logo Splendid.jpg";
 import creando from "../assets/Creando....png";
+import costura from "../assets/Web Costura.png";
 
 function Projects() {
   return (
@@ -17,36 +18,53 @@ function Projects() {
         <div className="lg:columns-4 sm:columns-2">
           <div className="p-5 bg-gray-50 sm:me-5 sm:mb-10 mb-5 rounded-md hover:shadow-md">
             <h3 className="text-xl mb-5">
-              <a href="https://github.com/ngpalma/rick_and_morty" target="_blank">Rick & Morty</a>
+              <a
+                href="https://github.com/ngpalma/rick_and_morty"
+                target="_blank"
+              >
+                Rick & Morty
+              </a>
             </h3>
             <img className="rounded-md h-48 max-h-48 w-full" src={rym} />
           </div>
 
           <div className="p-5 bg-gray-50 sm:me-5 sm:mb-10 mb-5 rounded-md hover:shadow-md">
             <h3 className="text-xl mb-5">
-              <a href="https://github.com/ngpalma/Countries" target="_blank">Info Paises</a>
+              <a href="https://github.com/ngpalma/Countries" target="_blank">
+                Info Paises
+              </a>
             </h3>
             <img className="rounded-md h-48 max-h-48 w-full" src={countries} />
           </div>
 
           <div className="p-5 bg-gray-50 sm:me-5 sm:mb-10 mb-5 rounded-md hover:shadow-md">
             <h3 className="text-xl mb-5">
-              <a href="https://github.com/ngpalma/clientToolVerse" target="_blank">Tool Verse</a>
+              <a
+                href="https://github.com/ngpalma/clientToolVerse"
+                target="_blank"
+              >
+                Tool Verse
+              </a>
             </h3>
             <img className="rounded-md h-48 max-h-48 w-full" src={toolverse} />
           </div>
 
           <div className="p-5 bg-gray-50 sm:me-5 sm:mb-10 mb-5 rounded-md hover:shadow-md">
             <h3 className="text-xl mb-5">
-              <a href="https://github.com/ngpalma/client-spLENDid-app" target="_blank">spLENDid - En curso</a>
+              <a
+                href="https://github.com/ngpalma/client-spLENDid-app"
+                target="_blank"
+              >
+                spLENDid - En desarrollo
+              </a>
             </h3>
             <img className="rounded-md h-48 max-h-48 w-full" src={splendid} />
           </div>
         </div>
         <div className="lg:columns-4 sm:columns-2">
           <div className="p-5 bg-gray-50 sm:me-5 sm:mb-10 mb-5 rounded-md hover:shadow-md">
-            <h3 className="text-xl mb-5">Reservado</h3>
-            <img className="rounded-md h-48 max-h-48 w-full" src={creando} />
+            <h3 className="text-xl mb-5">Web costura - En desarrollo</h3>
+            <img className="rounded-md h-48 max-h-48 w-full" src={costura} />
           </div>
           <div className="p-5 bg-gray-50 sm:me-5 sm:mb-10 mb-5 rounded-md hover:shadow-md">
             <h3 className="text-xl mb-5">Reservado</h3>
